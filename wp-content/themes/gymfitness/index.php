4aa3ad44fdaa2f404bbd76bb7d4555767bdd259b
AbrahamRamirez1997 <abraham@nubexo.com>
2026-07-27 19:33:37 +0200

    header

--- wp-content/themes/gymfitness/index.php
+++ wp-content/themes/gymfitness/index.php
@@ -1,9 +1,39 @@
-<?php
-    while( have_posts() ): the_post(); /* have_post interactua con la bbdd y the_post con las paginas */
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php wp_head();?>
+</head>
+<body>
+    <header class="header">
+        <div class="contenedor barra-navegacion">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="logotipo">
+            </div>
+            <?php
+                $args = array(
+                    'theme-location' =>'menu-principal',
+                    'container' => 'nav',
+                    'container_class' => 'menu-principal'
+                );
 
-    the_title();
+                wp_nav_menu($args);
+            ?>
+        </div>
+    </header>
+    <main>
+        <?php
+            while( have_posts() ): the_post(); /* have_post interactua con la bbdd y the_post con las paginas */
 
-    the_content();
-    
-    endwhile;
-?>
+            the_title();
+
+            the_content();
+            
+            endwhile;
+        ?> 
+    </main>
+
+</body>
+</html>
