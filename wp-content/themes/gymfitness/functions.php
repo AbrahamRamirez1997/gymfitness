1e6643f9a525b05803191ba55adbe2338a64e750
AbrahamRamirez1997 <abraham@nubexo.com>
2026-07-31 12:06:18 +0200

    footer terminado

--- wp-content/themes/gymfitness/functions.php
+++ wp-content/themes/gymfitness/functions.php
@@ -4,6 +4,9 @@ function gymfitness_setup() {
 
     // Imagenes Destacadas
     add_theme_support('post-thumbnails');
+
+    // Titulos para SEO
+    add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'gymfitness_setup');
 function gymfitness_menus() {
